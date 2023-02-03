4399cab6c8e36db506c0eb60ccaa6e72559fd2ac
Sanek <Leksok@list.ru>
2023-02-03 14:22:15 +0500

    пытался решить 2 задачу

--- 28.01.2023_C#1/4 дз/Program.cs
+++ 28.01.2023_C#1/4 дз/Program.cs
@@ -2,3 +2,22 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число с: ");
+int с = int.Parse(Console.ReadLine());
+int a,b,c;
+if(a > b && a > c){
+    ();
+}else{
+    ($"Большее число {max = b}");
+}
+if(max > c){
+    ($"Большее число {max = a}");
+}else{
+    ($"Большее число {max = c}");
+}
+Console.Write($"{max} ");
