32fdf0fcc74256c818ca494e7e28e10051bea922
Sanek <Leksok@list.ru>
2023-02-03 14:53:29 +0500

    Вроде решил 2 задачи из трех

--- 28.01.2023_C#1/6 дз/Program.cs
+++ 28.01.2023_C#1/6 дз/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+//(делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+if (b == a % 2){
+    Console.WriteLine($"Число {a} является четным");
+}else{
+    Console.WriteLine($"Число {a} не является четным");
+}
