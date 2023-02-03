7b43c38210046a9037ce52fbfec0407cef576ac0
Sanek <Leksok@list.ru>
2023-02-03 15:29:57 +0500

    Вроде решил 3 задачи из четырех

--- 28.01.2023_C#1/8 дз/Program.cs
+++ 28.01.2023_C#1/8 дз/Program.cs
@@ -1,3 +1,11 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+Console.Clear();
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+int b = 2;
+while(b <= n){
+    Console.Write($"{b} ");
+    b = b + 2;
+}
