4e59e60239111f10619284e1d0719e7045cec343
Sanek <Leksok@list.ru>
2023-02-03 08:00:15 +0500

    добавил строки

--- 28.01.2023_C#1/2 дз/Program.cs
+++ 28.01.2023_C#1/2 дз/Program.cs
@@ -2,3 +2,9 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (b == a * a){
