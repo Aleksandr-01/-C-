e6e3dd78b06f4e05ddb1aece5ea361619cccad5f
Sanek <Leksok@list.ru>
2023-02-04 14:02:29 +0500

    Записал еще пройденные задачи

---
+++ 28.01.2023_C#1/14/Program.cs
@@ -0,0 +1,10 @@
+﻿//7 и 23 число одновременно кратно
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 7 == 0 && num % 23 == 0) 
+{
+    Console.WriteLine("да");
+}
+else
+    Console.WriteLine("нет");
