9144d6c5c028ddf0e40aab1fc88190147abb74aa
Sanek <Leksok@list.ru>
2023-02-04 13:48:02 +0500

    Записал пройденные задачи

---
+++ 28.01.2023_C#1/12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (0 == num1 % num2)
+{
+    Console.WriteLine($"{num1}, {num2} -> Кратно, остатка нет");
+}
+else
+{
+    Console.WriteLine($"{num1}, {num2} -> Не кратно, остаток от деления {num1 % num2}");
+}
