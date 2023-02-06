24d7124b3f3e6951afe07cf308280d911b52f03e
Sanek <Leksok@list.ru>
2023-02-06 19:52:27 +0500

    решил задачу 19

--- 28.01.2023_C#1/19 дз/Program.cs
+++ 28.01.2023_C#1/19 дз/Program.cs
@@ -3,3 +3,22 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] & number[1]==number[3])
+  {
+    Console.WriteLine($"{number} - Это число палиндром");
+  }
+  else Console.WriteLine($"{number} - Это число не палиндром");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else
+{
+    Console.WriteLine($"Введено не пятизначное число");
+}
