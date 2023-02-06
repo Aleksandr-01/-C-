aab2faaac9351822124d1f3e1d8cd03b8e50c158
Sanek <Leksok@list.ru>
2023-02-06 15:36:26 +0500

    Добавид 20 задачу как на семинаре с проверкой

---
+++ 28.01.2023_C#1/20/Program.cs
@@ -0,0 +1,20 @@
+﻿// 20 Напишите программу,
+// которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве.
+// A (3,6); B(2, 1)-> 5,09
+// A(7, -5); B(1, -1)-> 7,21
+//sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
+//double s = Math.Sqrt(...);
+Console.Clear();
+Console.Write("Введите X1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"d={d:f3}");
