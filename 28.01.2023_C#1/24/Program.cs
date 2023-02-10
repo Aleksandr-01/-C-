28e21a80e94f2957d9c78ed8328cecc74d24fa92
Sanek <Leksok@list.ru>
2023-02-11 01:20:28 +0500

    Записал задачу 24

---
+++ 28.01.2023_C#1/24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число А
+// и выдает сумму чисел от 1 до А
+// a = 5
+// sum = 1 + 2 + 3 + 4 + 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int limit = int.Parse(Console.ReadLine());
+Console.WriteLine ($"Сумма чисел от 1 до {limit} равна {GetSum (limit)}");
+
+int GetSum (int limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)
+         sum += i;
+    return sum;
+}
