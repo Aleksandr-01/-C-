d4074e3ff4d590ae813c3e8d9183f429a3df4b61
Sanek <Leksok@list.ru>
2023-02-11 03:39:11 +0500

    Решил задачу 25

--- 28.01.2023_C#1/25 дз/Program.cs
+++ 28.01.2023_C#1/25 дз/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine());
+int n = POW(a, b);
+Console.WriteLine($"Число A в степени B = {n}");
+
+int POW (int x, int y)
+{
+    int pow = 1;
+    for(int i = 1; i <= y; i++)
+        {
+        pow *= x;
+        }
+    return pow;
+}
--- 28.01.2023_C#1/29 дз/Program.cs
+++ 28.01.2023_C#1/29 дз/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int[] array = Create(num);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] Create (int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+    return result;
+    }
