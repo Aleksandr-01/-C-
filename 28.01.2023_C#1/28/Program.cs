3dc6a1d3cba708bbca64e7214bba98af18998a8d
Sanek <Leksok@list.ru>
2023-02-11 02:08:45 +0500

    Записал задачу 28

---
+++ 28.01.2023_C#1/28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine ($"Произведение чисел = {GetMyltiply (n)}");
+
+int GetMyltiply (int n)
+{
+    int result = 1;
+    for (int i =2; i <= n; i++)
+        result *= i;
+    return result;
+}
