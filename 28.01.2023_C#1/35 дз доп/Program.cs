6a7d4d14f4dcad53d8ebea029dce78b297b21c8c
Sanek <Leksok@list.ru>
2023-02-18 03:03:41 +0500

    Решил задачу 35

---
+++ 28.01.2023_C#1/35 дз доп/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 35:Задайте одномерный массив из 123 случайных чисел [0: 1000].
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2]-> 0
+// [10, 11, 12, 13, 14]-> 5
+
+Console.Clear();
+
+int [] RandomArray(int n)
+{   
+    int [] array = new int [n];
+        for(int i = 0; i < n; i++)
+    {
+        array [i] = new Random().Next(1000);
+    }
+    return array;
+}
+
+int [] arr1 = RandomArray(123);
+
+int FindNum(int[] arr)
+{
+int find = 0;
+foreach(int x in arr)
+{
+    find += x > 9 && x <100 ? 1 : 0;
+}
+
+return find;
+}
+
+Console.Write($"[{String.Join(", ", arr1)}]");
+Console.Write ($" -> {FindNum (arr1)}");
