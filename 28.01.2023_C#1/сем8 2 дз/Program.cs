0ea1270580fe3fb3ddd5922f28563becb1214b5a
Sanek <Leksok@list.ru>
2023-02-26 03:34:29 +0500

    Решил задчу 2 сем8

---
+++ 28.01.2023_C#1/сем8 2 дз/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 2: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк: ");
+int m = int.Parse(ReadLine());
+Write("Введите количество столбцов: ");
+int n = int.Parse(ReadLine());
+int[,] arrayX =  GetArray(m, n);
+int[,] arrayY =  GetArray(m, n);
+int[,] result =  MultArray(arrayX, arrayY);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(10);
+        }
+
+    }
+    return result;
+}
+
+int[,] MultArray(int [,] A, int [,] B)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = A[i,j] * B[i,j];
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+PrintArray(arrayX);
+WriteLine();
+PrintArray(arrayY);
+WriteLine();
+PrintArray(result);
