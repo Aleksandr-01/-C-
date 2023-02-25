b839fc0813820ca7255adc6a43d517ba02ba276b
Sanek <Leksok@list.ru>
2023-02-26 03:02:53 +0500

    Решил задачу 61

---
+++ 28.01.2023_C#1/61 дз/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 61
+// Показать треугольник Паскаля. *Сделать вывод в виде равнобедренного треугольника.
+
+Console.Clear();
+int i, n, c;
+Console.WriteLine("Введите количество строк треугольника Паскаля:");
+string s = Console.ReadLine();
+n = Convert.ToInt32(s);
+
+float factorial(int n){
+   float x = 1;
+   for (int i = 1; i <= n; i++) x *= i;
+   return x;
+}
+
+for (i = 0; i < n; i++){
+   for (c = 0; c <= (n - i); c++)
+      Console.Write(" "); 
+   for (c = 0; c <= i; c++){
+      Console.Write(" ");
+      Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); 
+   }
+   Console.WriteLine(); 
+}
