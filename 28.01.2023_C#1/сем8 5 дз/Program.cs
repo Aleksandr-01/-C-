b9d3e8c3c5ab32d10e98c15396b86ff34176fb36
Sanek <Leksok@list.ru>
2023-02-26 04:33:16 +0500

    Решил задчу 5 сем8

---
+++ 28.01.2023_C#1/сем8 5 дз/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 5. Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+Console.Clear();
+Console.Write("Введите начальную длинну сторон спирали: ");
+int a = int.Parse(Console.ReadLine());
+int[,] b = new int[a, a];
+
+void FillArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArraySpiral(b, a);
+PrintArray(b);
